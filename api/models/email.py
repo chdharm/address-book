2137315fd3ae364fccaff9b04682b9c22ca42241
Dharmpal Chaudhary <dharmpalchaudhary@Admins-MacBook-Pro.local>
2021-03-26 02:55:20 +0530

    added indexes

--- api/models/email.py
+++ api/models/email.py
@@ -4,4 +4,4 @@ from .person import Person
 
 class Email(models.Model):
     person = models.ForeignKey(Person, editable=False, on_delete=models.CASCADE)
-    email = models.EmailField(blank=True, max_length=70, unique=True)
+    email = models.EmailField(blank=True, max_length=70, unique=True, db_index=True)
