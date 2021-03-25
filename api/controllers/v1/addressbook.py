1253f10d45623ae9700afbf13251ec18733dfb44
Dharmpal Chaudhary <dharmpalchaudhary@Admins-MacBook-Pro.local>
2021-03-25 20:17:53 +0530

    added  proper endpoint fundtions

---
+++ api/controllers/v1/addressbook.py
@@ -0,0 +1,77 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(['POST'])
+def add_person(request):
+    """
+    This end point will take the person 
+    data and will create their record 
+    into the database.
+
+    """
+    pass
+
+
+@api_view(['POST'])
+def search_person(request):
+    """
+    This Endpoint is for searching contact 
+    details of a person based on their 
+    email id or name.
+
+    """
+    pass
+
+
+@api_view(['POST'])
+def delete(request):
+    """
+    This Endpoint will delete 
+    the person contact details. 
+    It expects personId in URL param.
+
+    """
+    pass
+
+@api_view(['POST'])
+def edit(request):
+    """
+    This Endpoint will edit the person 
+    contact details. 
+    It expects personId in URL param.
+
+    """
+    pass
+
+
+@api_view(['POST'])
+def add_mobile(request):
+    """
+    This Endpoint will add the mobile number 
+    into the person contact details. 
+    It expects personId in URL param.
+
+    """
+    pass
+
+@api_view(['POST'])
+def add_email(request):
+    """
+    This Endpoint will add the email id into 
+    the person contact details. 
+    It expects personId in URL param.
+
+    """
+    pass
+
+@api_view(['POST'])
+def add_address(request):
+    """
+    This Endpoint will add the address into 
+    the person contact details. 
+    It expects personId in URL param.
+
+    """
+    pass
--- api/urls/v1/urls.py
+++ api/urls/v1/urls.py
@@ -3,11 +3,11 @@ from api.views.v1 import views
 
 urlpatterns =[
     path('health/', views.health, name='health'),
-    # path('addPerson/', , name='add_person'),
-    # path('searchPerson/', , name='search_person'),
-    # re_path(r'delete/(?P<pk>[0-9]+$)', , name='delete_person_contact'),
-    # re_path(r'edit/(?P<pk>[0-9]+$)', , name='edit_person_contact'),
-    # re_path(r'addMobile/(?P<pk>[0-9]+$)', , name='add_person_mobile'),
-    # re_path(r'addEmail/(?P<pk>[0-9]+$)', , name='add_person_email'),
-    # re_path(r'addAddress/(?P<pk>[0-9]+$)', , name='add_person_address'),
+    path('addPerson/', , name='add_person'),
+    path('searchPerson/', , name='search_person'),
+    re_path(r'delete/(?P<pk>[0-9]+$)', , name='delete_person_contact'),
+    re_path(r'edit/(?P<pk>[0-9]+$)', , name='edit_person_contact'),
+    re_path(r'addMobile/(?P<pk>[0-9]+$)', , name='add_person_mobile'),
+    re_path(r'addEmail/(?P<pk>[0-9]+$)', , name='add_person_email'),
+    re_path(r'addAddress/(?P<pk>[0-9]+$)', , name='add_person_address'),
 ]
